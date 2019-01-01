0f7174be6671f40ff44fff7327e655082e85e7c9
Faisal Ahmed <faisal@ahmedmail.info>
2019-01-01 13:45:25 +0000

    add subgroup infrastructure

---
+++ Src/ShopperHolic.Infrastructure/ShopperHolicDTO/Stock/SubGroups/SubGroupDTO.cs
@@ -0,0 +1,11 @@
+namespace ShopperHolic.Infrastructure.ShopperHolicDTO
+{
+    public class SubGroupDTO
+    {
+        public int SubGroupID {get; set;}
+        public string SubGroupCode {get;set;}
+        public string SubGroupName {get;set;}
+        public string SubGroupDescription {get;set;}
+        public int ProductGroupID {get;set;}
+    }
+}
---
+++ Src/ShopperHolic.Infrastructure/ShopperHolicDTO/Stock/SubGroups/SubGroupPreviewDTO.cs
@@ -0,0 +1,9 @@
+namespace ShopperHolic.Infrastructure.ShopperHolicDTO
+{
+    public class SubGroupPreviewDTO
+    {
+        public int SubGroupID {get; set;}
+        public string SubGroupCode {get;set;}
+        public string SubGroupName {get;set;}
+    }
+}
