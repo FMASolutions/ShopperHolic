8c9bb01b3d46e9f4058fe67ca8e729c7864de862
Faisal Ahmed <faisal@ahmedmail.info>
2019-01-06 15:02:02 +0000

    move logic to service in web ui

--- Src/ShopperHolic.WebUI/ShopperHolic/src/app/components/stock/product-groups/product-group-list/product-group-list.component.ts
+++ Src/ShopperHolic.WebUI/ShopperHolic/src/app/components/stock/product-groups/product-group-list/product-group-list.component.ts
@@ -12,25 +12,17 @@ import { Globals } from 'src/globals';
 })
 export class ProductGroupListComponent implements OnInit {
 
-  tableDataSource: MatTableDataSource<ProductGroupPreview>;
-  textFilter: string = "";
-  displayedColumns: string[] = Globals.PROD_GROUP_PRVW_LIST_COLUMNS;
-
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
 
-  constructor(private prodGroupService: ProductGroupService, public prodDialog: MatDialog) { }
+  constructor(public prodGroupService: ProductGroupService, public prodDialog: MatDialog) { }
 
-  ngOnInit() {
-    setTimeout(()=>{ 
-      this.refreshDatasource();
-    },1); 
-  }
+  ngOnInit() { setTimeout(() => { this.prodGroupService.refreshListData(this.paginator); }, 1); }
 
   public createClicked() {
     let dialogRef = this.prodDialog.open(ProductGroupComponent, Globals.APP_SETTINGS.DEFAULT_MODAL_SETTINGS);
-    let obs = dialogRef.afterClosed().subscribe((resp) => { 
-      if (resp && resp.userSubmitted) { this.refreshDatasource(); }
+    let obs = dialogRef.afterClosed().subscribe((resp) => {
+      if (resp && resp.userSubmitted) { this.prodGroupService.refreshListData(this.paginator); }
       obs.unsubscribe();
     });
   }
@@ -40,67 +32,24 @@ export class ProductGroupListComponent implements OnInit {
     modalSettings.data = id;
 
     let dialogRef = this.prodDialog.open(ProductGroupComponent, modalSettings);
-    let obs = dialogRef.afterClosed().subscribe((resp) => { 
-      if (resp && resp.userSubmitted) { this.refreshDatasource(); }
-      obs.unsubscribe(); 
+    let obs = dialogRef.afterClosed().subscribe((resp) => {
+      if (resp && resp.userSubmitted) { this.prodGroupService.refreshListData(this.paginator); }
+      obs.unsubscribe();
     });
   }
 
   public deleteClicked(id: number) {
     if (confirm(Globals.PROD_GROUP_DELETE_CONFIRM_MSG + id)) {
-      let obs = this.prodGroupService.delete(id).subscribe(() => { 
-        this.refreshDatasource();
-        obs.unsubscribe(); 
+      let obs = this.prodGroupService.delete(id).subscribe(() => {
+        this.prodGroupService.refreshListData(this.paginator);
+        obs.unsubscribe();
       });
     }
   }
 
-  public resetFilterClicked() {
-    this.prodGroupService.textFilter = "";
-    /*
-    this.textFilter = "";
-    */
-    this.applyFilterClicked();
-  }
+  public resetFilterClicked() { this.prodGroupService.resetListFilter() }
 
   public applyFilterClicked() { this.prodGroupService.applyListFilter() }
 
-  public sortClicked(sort: Sort) {
-    this.prodGroupService.sortTableData(sort, this.paginator);
-    /*
-    const data = this.tableDataSource.filteredData.slice();
-    if (!sort.active || sort.direction === '') {
-      return;
-    }
-    let sortedData = data.sort((a, b) => {
-      const isAsc = sort.direction === 'asc';
-      switch (sort.active) {
-        case 'ID': return this.compare(a.productGroupID, b.productGroupID, isAsc);
-        case 'Code': return this.compare(a.productGroupCode.toLowerCase(), b.productGroupCode.toLowerCase(), isAsc);
-        case 'Name': return this.compare(a.productGroupName.toLowerCase(), b.productGroupName.toLowerCase(), isAsc);
-        default: return 0;
-      }
-    });
-    this.setupTableDataSource(sortedData);
-    */
-  }
-  /*
-  private setupTableDataSource(data: ProductGroupPreview[]) {
-    this.tableDataSource = new MatTableDataSource(data);
-    this.tableDataSource.paginator = this.paginator;
-    this.tableDataSource.filter = this.textFilter;
-  }
-  
-
-  private compare(a: number | string, b: number | string, isAsc: boolean) { return (a < b ? -1 : 1) * (isAsc ? 1 : -1); }
-*/
-  private refreshDatasource() {
-    this.prodGroupService.refreshListData(this.paginator);
-    /*
-    let obs = this.prodGroupService.getAll().subscribe(prodGroupsResp => {
-      this.setupTableDataSource(prodGroupsResp);
-      obs.unsubscribe();
-    })
-    */
-  }
+  public sortClicked(sort: Sort) { this.prodGroupService.sortTableData(sort, this.paginator); }
 }
