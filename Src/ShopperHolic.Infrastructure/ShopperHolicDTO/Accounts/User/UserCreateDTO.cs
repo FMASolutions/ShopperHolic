4cfdf20fd18dfbd6d7c8b802ff841ff23ccb21c0
Faisal Ahmed <faisal@ahmedmail.info>
2019-01-07 00:57:07 +0000

    Update todo and soome skeleton user account DTO's

---
+++ Src/ShopperHolic.Infrastructure/ShopperHolicDTO/Accounts/User/UserCreateDTO.cs
@@ -0,0 +1,9 @@
+namespace ShopperHolic.Infrastructure.ShopperHolicDTO
+{
+    public class UserCreateDTO
+    {
+        public string Username {get;set;}
+        public string EmailAddress {get;set;}
+        public string Password {get;set;}
+    }
+}
---
+++ Src/ShopperHolic.Infrastructure/ShopperHolicDTO/Accounts/User/UserDTO.cs
@@ -0,0 +1,12 @@
+namespace ShopperHolic.Infrastructure.ShopperHolicDTO
+{
+    /* ALREADY IMPLEMENTED AS UserProfileDTO within Security
+    public class UserProfileDTO
+    {
+        public int UserID {get;set;}
+        public string Username {get;set;}
+        public string EmailAddress {get;set;}
+        public string EncryptedPassword {get;set;}
+    }
+     */
+}
