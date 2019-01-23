f401b9345dd5d216354e3a95573c87ec323eb38a
FMASolutions <faisal@ahmedmail.info>
2019-01-23 13:04:05 +0000

    Update OrderRepo.cs

---
+++ Src/ShopperHolic.Persistence/ShopperHolicDataProvider/Repositories/OrderProcessing/OrderRepo.cs
@@ -0,0 +1,47 @@
+using System;
+using Dapper;
+using System.Data;
+using System.Collections.Generic;
+using ShopperHolic.Infrastructure.ShopperHolicDTO;
+using ShopperHolic.Infrastructure.ShopperExceptions;
+
+namespace ShopperHolic.Persistence.ShopperHolicDataProvider.Repositories
+{
+    public class OrderRepo : BaseRepo, IOrderRepo
+    {
+        public OrderRepo(IDbTransaction transaction) : base(transaction) { }
+        
+        public int Create(CreateOrderDTO entityToCreate)
+        {
+          throw new NotImplementedException();
+        }
+        public OrderDTO GetByID(int id)
+        {
+          throw new NotImplementedException();
+        }
+        public IEnumerable<OrderPreviewDTO> GetAllPreview()
+        {
+          throw new NotImplementedException();
+        }
+        public OrderDTO Update(OrderDTO updatedRecord)
+        {
+          throw new NotImplementedException();
+        }
+        public bool Delete(int id)
+        {
+          throw new NotImplementedException();
+        }
+        public IEnumerable<OrderItemDTO> GetItemsForOrder(int id)
+        {
+          throw new NotImplementedException();
+        }
+        public OrderItemDTO AddItemToOrder(CreateOrderItemDTO entityToCreate)
+        {
+          throw new NotImplementedException();
+        }
+        public bool RemoveItemFromOrder(int orderItemID)
+        {
+          throw new NotImplementedException();
+        }
+    }
+}
