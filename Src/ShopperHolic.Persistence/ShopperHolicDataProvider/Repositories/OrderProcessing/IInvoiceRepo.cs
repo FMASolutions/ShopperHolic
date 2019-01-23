274695b2af62fc1f428c5a3d7d928b068e5f483b
FMASolutions <faisal@ahmedmail.info>
2019-01-23 09:44:45 +0000

    Update IInvoiceRepo.cs

---
+++ Src/ShopperHolic.Persistence/ShopperHolicDataProvider/Repositories/OrderProcessing/IInvoiceRepo.cs
@@ -0,0 +1,13 @@
+using ShopperHolic.Infrastructure.ShopperHolicDTO;
+using System.Collections.Generic;
+
+namespace ShopperHolic.Persistence.ShopperHolicDataProvider.Repositories
+{
+    public interface IInvoiceRepo
+    {
+        int InvoiceOrder(int orderID);
+        InvoiceDTO GetByID(int id);
+        IEnumerable<InvoicePreviewDTO> GetAllPreview();       
+        bool Delete(int id);
+    }
+}
